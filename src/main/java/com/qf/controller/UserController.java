5545bc6a5a39be6f89f74e6b7ae1f860d45bbea8
18821710272 <74186877+18821710272@users.noreply.github.com>
2020-11-09 20:26:22 +0800

    Update UserController.java
    删除UserController的doPost方法

--- src/main/java/com/qf/controller/UserController.java
+++ src/main/java/com/qf/controller/UserController.java
@@ -21,9 +21,4 @@ public class UserController extends HttpServlet {
         req.setAttribute("users",users);
         req.getRequestDispatcher("list.jsp").forward(req,resp);
     }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
 }
